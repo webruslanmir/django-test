45f1de625f2c22c7bf9670679f5a960ecadb0044
ruslanmir <webruslanmir@gmail.com>
2021-08-22 11:57:24 +0300

    test1

---
+++ testproject/testproject/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-)rqybi3##8cyn@!q9_m3_5am0lsku$$@na#sub#$!tsttmt1b#'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
